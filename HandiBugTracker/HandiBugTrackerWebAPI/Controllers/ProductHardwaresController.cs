d49640f9a41dd9d062bc74fe18e388a649e1748f
Handinata Tanudjaja <handi.tan@gmail.com>
2020-08-13 00:44:05 -0500

    Add ProductHardwaresController
    Added API for get api/producthardwares

---
+++ HandiBugTracker/HandiBugTrackerWebAPI/Controllers/ProductHardwaresController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using HandiBugTrackerDataManager.DataAccess;
+using HandiBugTrackerDataManager.Models;
+
+namespace HandiBugTrackerWebAPI.Controllers
+{
+    [Authorize]
+    public class ProductHardwaresController : ApiController
+    {
+        private readonly IProductHardwareData _productHardwareData;
+
+        public ProductHardwaresController() { }
+        public ProductHardwaresController(IProductHardwareData pProductHardwareData)
+        {
+            this._productHardwareData = pProductHardwareData;
+        }
+        
+        //GET api/ProductHardwares
+        public async Task<IEnumerable<ProductHardwareModel>> Get()
+        {
+            var result = await _productHardwareData.GetProductHardwares();
+            return result;
+        }
+    }
+}
