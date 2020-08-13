86a5e0db5e88c8e0795a499358570def1222de54
Handinata Tanudjaja <handi.tan@gmail.com>
2020-08-13 00:41:09 -0500

    Add IoC Container
    Implement IoC via Autofac

---
+++ HandiBugTracker/HandiBugTrackerWebAPI/App_Start/ContainerConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using HandiBugTrackerDataManager.DataAccess;
+using HandiBugTrackerDataManager.Internal.DataAccess;
+
+namespace HandiBugTrackerWebAPI.App_Start
+{
+    public class ContainerConfig
+    {
+        internal static void RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+            //For Web API, we use RegisterApiControllers
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<SqlDataAccess>().
+                   As<ISqlDataAccess>().
+                   InstancePerRequest();
+
+            builder.RegisterType<ProductHardwareData>().
+                    As<IProductHardwareData>().
+                    InstancePerRequest();
+
+            var container = builder.Build();
+            //For Web API, we use GlobalConfiguration
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
