e629027e9d0a01f93e2d39153f8c778a88da0845
Handinata Tanudjaja <handi.tan@gmail.com>
2020-08-13 22:56:50 -0500

    Register types moved to a function
    Have a private function to do all the types registration

--- HandiBugTracker/HandiBugTrackerWebAPI/App_Start/ContainerConfig.cs
+++ HandiBugTracker/HandiBugTrackerWebAPI/App_Start/ContainerConfig.cs
@@ -19,17 +19,70 @@ namespace HandiBugTrackerWebAPI.App_Start
             //For Web API, we use RegisterApiControllers
             builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
 
-            builder.RegisterType<SqlDataAccess>().
-                   As<ISqlDataAccess>().
-                   InstancePerRequest();
-
-            builder.RegisterType<ProductHardwareData>().
-                    As<IProductHardwareData>().
-                    InstancePerRequest();
+            registerTypes(builder);
 
             var container = builder.Build();
             //For Web API, we use GlobalConfiguration
             GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        private static void registerTypes(ContainerBuilder pBuilder)
+        {
+            pBuilder.RegisterType<BugCommentData>().
+                    As<IBugCommentData>().
+                    InstancePerRequest();
+
+            pBuilder.RegisterType<BugPriorityData>().
+                    As<IBugPriorityData>().
+                    InstancePerRequest();
+
+            pBuilder.RegisterType<BugSeverityData>().
+                    As<IBugSeverityData>().
+                    InstancePerRequest();
+
+            pBuilder.RegisterType<BugStatusData>().
+                    As<IBugStatusData>().
+                    InstancePerRequest();
+
+            pBuilder.RegisterType<BugStatusSubStateData>().
+                    As<IBugStatusSubStateData>().
+                    InstancePerRequest();
+
+            pBuilder.RegisterType<BugTypeData>().
+                    As<IBugTypeData>().
+                    InstancePerRequest();
+
+            pBuilder.RegisterType<ComponentBugData>().
+                    As<IComponentBugData>().
+                    InstancePerRequest();
+
+            pBuilder.RegisterType<ComponentData>().
+                    As<IComponentData>().
+                    InstancePerRequest();
+
+            pBuilder.RegisterType<UserData>().
+                    As<IUserData>().
+                    InstancePerRequest();
+
+            pBuilder.RegisterType<ProductVersionData>().
+                    As<IProductVersionData>().
+                    InstancePerRequest();
+
+            pBuilder.RegisterType<ProductOSData>().
+                    As<IProductOSData>().
+                    InstancePerRequest();
+
+            pBuilder.RegisterType<ProductData>().
+                    As<IProductData>().
+                    InstancePerRequest();
+
+            pBuilder.RegisterType<ProductHardwareData>().
+                    As<IProductHardwareData>().
+                    InstancePerRequest();
+
+            pBuilder.RegisterType<SqlDataAccess>().
+                   As<ISqlDataAccess>().
+                   InstancePerRequest();
         }
     }
 }
