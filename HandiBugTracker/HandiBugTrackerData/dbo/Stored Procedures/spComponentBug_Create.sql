0f98cb6b8f3388f8bcda1fabd2916b9eadafe8cc
Handinata Tanudjaja <handi.tan@gmail.com>
2020-08-15 10:25:55 -0500

    Added SP CreateBug and alter User column
    SP CreateBug is now available and corrected some column's data type

---
+++ HandiBugTracker/HandiBugTrackerData/dbo/Stored Procedures/spComponentBug_Create.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [dbo].[spComponentBug_Create]
+	@Name NVARCHAR (256),
+	@ReporterId NVARCHAR (128),
+	@AssigneeId NVARCHAR (128),
+	@QAContactId NVARCHAR (128),
+	@ProductId int,
+	@ProductVersionId int,
+	@BugStatusId int,
+	@BugStatusSubStateId int,
+	@BugPriorityId int,
+	@BugSeverityId int,
+	@BugTypeId int,
+	@ProductHardwareId int,
+	@ProductOSId int,
+	@ComponentId int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO dbo.ComponentBug 
+	([Name],ReporterId,AssigneeId,QAContactId,
+	 ProductId,ProductVersionId,BugStatusId,
+	 BugStatusSubStateId,BugPriorityId,BugSeverityId,BugTypeId,
+	 ProductHardwareId,ProductOSId,ComponentId)
+	 VALUES
+	 (@Name,@ReporterId,@AssigneeId,@QAContactId,
+	  @ProductId,@ProductVersionId,@BugStatusId,
+	  @BugStatusSubStateId,@BugPriorityId,@BugSeverityId,@BugTypeId,
+	  @ProductHardwareId,@ProductOSId,@ComponentId)
+END
--- HandiBugTracker/HandiBugTrackerData/dbo/Tables/User.sql
+++ HandiBugTracker/HandiBugTrackerData/dbo/Tables/User.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[User]
 (
 	[Id] NVARCHAR(128) NOT NULL PRIMARY KEY DEFAULT NEWID(), 
-    [FirstName] NCHAR(10) NOT NULL, 
-    [LastName] NCHAR(10) NOT NULL, 
-    [EmailAddress] NCHAR(10) NOT NULL, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [EmailAddress] NVARCHAR(256) NOT NULL, 
     [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
     [LastModifiedDate] DATETIME2 NOT NULL DEFAULT getutcdate()
 )
