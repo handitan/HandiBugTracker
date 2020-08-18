b7940534480f72cb88e7c52aa4b219544fbb2c17
Handinata Tanudjaja <handi.tan@gmail.com>
2020-08-18 12:45:19 -0500

    Add more props, and displayname
    Added getter props to return selectListItem, new props along, and displaynames

--- HandiBugTracker/HandiBugTrackerWebClient.Library/Models/CompBugOptionsViewModel.cs
+++ HandiBugTracker/HandiBugTrackerWebClient.Library/Models/CompBugOptionsViewModel.cs
@@ -10,8 +10,6 @@ namespace HandiBugTrackerWebClient.Library.Models
     public class CompBugOptionsViewModel
     {
         public List<BugPriorityViewModel> BugPriorityList { get; set; }
-
-        //TODO??? Where the heck is bug priority in CompBug
         public IEnumerable<SelectListItem> BugPrioritySelectList
         {
             get
@@ -24,6 +22,18 @@ namespace HandiBugTrackerWebClient.Library.Models
             }
         }
         public List<BugSeverityViewModel> BugSeverityList { get; set; }
+        public IEnumerable<SelectListItem> BugSeveritySelectList
+        {
+            get
+            {
+                return BugSeverityList.Select(x => new SelectListItem
+                {
+                    Value = x.Id.ToString(),
+                    Text = x.Name
+                });
+            }
+        }
+
         public List<BugStatusViewModel> BugStatusList { get; set; }
         public IEnumerable<SelectListItem> BugStatusSelectList
         {
@@ -37,11 +47,94 @@ namespace HandiBugTrackerWebClient.Library.Models
             }
         }
         public List<BugStatusSubStateViewModel> BugStatusSubStateList { get; set; }
+        public IEnumerable<SelectListItem> BugStatusSubStateSelectList
+        {
+            get
+            {
+                return BugStatusSubStateList.Select(x => new SelectListItem
+                {
+                    Value = x.Id.ToString(),
+                    Text = x.Name
+                });
+            }
+        }
+
         public List<BugTypeViewModel> BugTypeList { get; set; }
+        public IEnumerable<SelectListItem> BugTypeSelectList
+        {
+            get
+            {
+                return BugTypeList.Select(x => new SelectListItem
+                {
+                    Value = x.Id.ToString(),
+                    Text = x.Name
+                });
+            }
+        }
+
         public List<ProductViewModel> ProductList { get; set; }
+        public IEnumerable<SelectListItem> ProductSelectList
+        {
+            get
+            {
+                return ProductList.Select(x => new SelectListItem
+                {
+                    Value = x.Id.ToString(),
+                    Text = x.Name
+                });
+            }
+        }
+
         public List<ProductHardwareViewModel> ProductHardwareList { get; set; }
+        public IEnumerable<SelectListItem> ProductHwSelectList
+        {
+            get
+            {
+                return ProductHardwareList.Select(x => new SelectListItem
+                {
+                    Value = x.Id.ToString(),
+                    Text = x.Name
+                });
+            }
+        }
+
         public List<ProductOSViewModel> ProductOSList { get; set; }
+        public IEnumerable<SelectListItem> ProductOSSelectList
+        {
+            get
+            {
+                return ProductOSList.Select(x => new SelectListItem
+                {
+                    Value = x.Id.ToString(),
+                    Text = x.Name
+                });
+            }
+        }
+
         public List<ComponentViewModel> ComponentList { get; set; }
+        public IEnumerable<SelectListItem> ComponentSelectList
+        {
+            get
+            {
+                return ComponentList.Select(x => new SelectListItem
+                {
+                    Value = x.Id.ToString(),
+                    Text = x.Name
+                });
+            }
+        }
+
         public List<ProductVersionViewModel> ProductVersionList { get; set; }
+        public IEnumerable<SelectListItem> ProductVersionSelectList
+        {
+            get
+            {
+                return ProductVersionList.Select(x => new SelectListItem
+                {
+                    Value = x.Id.ToString(),
+                    Text = x.Name
+                });
+            }
+        }
     }
 }
--- HandiBugTracker/HandiBugTrackerWebClient.Library/Models/CompBugViewModel.cs
+++ HandiBugTracker/HandiBugTrackerWebClient.Library/Models/CompBugViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,15 +17,33 @@ namespace HandiBugTrackerWebClient.Library.Models
         public string ProductName { get; set; }
         public int CompId { get; set; }
         public string CompName { get; set; }
+
+        public string ReporterId { get; set; }
+        public string ReporterName { get; set; }
+        public string AssigneeId { get; set; }
         public string AssigneeName { get; set; }
+        public string QAId { get; set; }
+        public string QAName { get; set; }
+
+        [Display(Name="Version")]
+        public int ProdVerId { get; set; }
+        [Display(Name = "Priority")]
+        public int BugPriorityId { get; set; }
+        [Display(Name = "Severity")]
+        public int BugSeverityId { get; set; }
+        [Display(Name = "Hardware")]
+        public int ProductHwId { get; set; }
+        [Display(Name = "OS")]
+        public int ProductOSId { get; set; }
+
         public int StatusId { get; set; }
         public string StatusName { get; set; }
         public int SubStateId { get; set; }
         public string SubStateName { get; set; }
         public DateTime LastModifiedDate { get; set; }
-        public IList<BugCommentViewModel> BugComments { get; set; }
 
         //client side
+        public IList<BugCommentViewModel> BugComments { get; set; }
         public string ClientNewComment { get; set; }
     }
 }
