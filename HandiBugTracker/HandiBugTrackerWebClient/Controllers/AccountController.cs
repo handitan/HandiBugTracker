4e3c28b381ea6b77428f6d378a7a6bfbc47af9a9
Handinata Tanudjaja <handi.tan@gmail.com>
2020-08-19 20:45:06 -0500

    Update AccountController
    Saved userEndPoint and peopleViewModel

--- HandiBugTracker/HandiBugTrackerWebClient/Controllers/AccountController.cs
+++ HandiBugTracker/HandiBugTrackerWebClient/Controllers/AccountController.cs
@@ -12,11 +12,18 @@ namespace HandiBugTrackerWebClient.Controllers
     public class AccountController : Controller
     {
         private readonly IAPIHelper _apiHelper;
+        private readonly IUsersEndpoint _userEndPoint;
+        private readonly PeopleViewModel _peopleViewModel;
         public AccountController() { }
-        public AccountController(IAPIHelper pIAPIHelper)
+        public AccountController(IAPIHelper pIAPIHelper,
+            IUsersEndpoint pUserEndPoint,
+            PeopleViewModel pPeopleViewModel)
         {
             _apiHelper = pIAPIHelper;
+            _userEndPoint = pUserEndPoint;
+            _peopleViewModel = pPeopleViewModel;
         }
+
         // GET: Login
         public ActionResult Index()
         {
@@ -32,6 +39,21 @@ namespace HandiBugTrackerWebClient.Controllers
                 var success = await _apiHelper.VerifyLogin(pAcctViewModel.LoginName, pAcctViewModel.Password);
                 if (success)
                 {
+                    _peopleViewModel.LoggedInEmailAddress = pAcctViewModel.LoginName;
+
+                    //TODO: need to revisit later to get user Id more efficiently
+                    var userList = await _userEndPoint.GetAll();
+                    foreach (var user in userList)
+                    {
+                        if (user.EmailAddress.Equals(_peopleViewModel.LoggedInEmailAddress))
+                        {
+                            _peopleViewModel.LoggedInUserId = user.Id;
+                            _peopleViewModel.LoggedInFirstName = user.FirstName;
+                            _peopleViewModel.LoggedInLastName = user.LastName;
+                            break;
+                        }
+                    }
+
                     return RedirectToRoute("Main");
                 }
             }
